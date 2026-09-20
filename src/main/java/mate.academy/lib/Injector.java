e938ba930ae7166dcdf1f68ef87bd315c15089d8
Dmytro Sakharov <dmytro.sakharov7615@gmail.com>
2026-09-20 15:36:46 +0300

    add injector class

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,35 +1,45 @@
 package mate.academy.lib;
 
+import mate.academy.service.FileReaderService;
+import mate.academy.service.ProductParser;
+import mate.academy.service.ProductService;
+import mate.academy.service.impl.FileReaderServiceImpl;
+import mate.academy.service.impl.ProductParserImpl;
+import mate.academy.service.impl.ProductServiceImpl;
 import java.lang.reflect.Field;
+import java.util.Map;
 
 public class Injector {
     private static final Injector injector = new Injector();
+    private Map<Class<?>, Class<?>> implementations = Map.of(
+            ProductService.class, ProductServiceImpl.class,
+            ProductParser.class, ProductParserImpl.class,
+            FileReaderService.class, FileReaderServiceImpl.class);
 
     public static Injector getInjector() {
-
         return injector;
     }
 
     public Object getInstance(Class<?> interfaceClazz) throws NoSuchMethodException {
-        if (!interfaceClazz.isAnnotationPresent(Component.class)) {
-            throw new RuntimeException("Class " + interfaceClazz.getName()
+        Class<?> implClass = implementations.getOrDefault(interfaceClazz, interfaceClazz);
+        if (!implClass.isAnnotationPresent(Component.class)) {
+            throw new RuntimeException("Class " + implClass.getName()
                     + " doesn't have @Component annotation");
         }
         try {
-            Object instance = interfaceClazz.getDeclaredConstructor().newInstance();
-            for (Field field : interfaceClazz.getDeclaredFields()) {
+            Object instance = implClass.getDeclaredConstructor().newInstance();
+            for (Field field : implClass.getDeclaredFields()) {
                 if (field.isAnnotationPresent(Inject.class)) {
                     Class<?> dependencyType = field.getType();
                     Object dependency = getInstance(dependencyType);
                     field.setAccessible(true);
                     field.set(instance, dependency);
                 }
-
             }
             return instance;
-        } catch (Exception e) {
+        } catch (ReflectiveOperationException e) {
             throw new RuntimeException("Failed to instantiate "
-                    + interfaceClazz.getName(), e);
+                    + implClass.getName(), e);
         }
     }
 }
