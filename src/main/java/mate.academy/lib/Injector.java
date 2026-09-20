bda67694231275127c7dd5851f3d09476b1c2494
Dmytro Sakharov <dmytro.sakharov7615@gmail.com>
2026-09-20 14:52:26 +0300

    add injector class

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,13 +1,35 @@
 package mate.academy.lib;
 
+import java.lang.reflect.Field;
+
 public class Injector {
     private static final Injector injector = new Injector();
 
     public static Injector getInjector() {
+
         return injector;
     }
 
-    public Object getInstance(Class<?> interfaceClazz) {
-        return null;
+    public Object getInstance(Class<?> interfaceClazz) throws NoSuchMethodException {
+        if (!interfaceClazz.isAnnotationPresent(Component.class)) {
+            throw new RuntimeException("Class " + interfaceClazz.getName()
+                    + " doesn't have @Component annotation");
+        }
+        try {
+            Object instance = interfaceClazz.getDeclaredConstructor().newInstance();
+            for (Field field : interfaceClazz.getDeclaredFields()) {
+                if (field.isAnnotationPresent(Inject.class)) {
+                    Class<?> dependencyType = field.getType();
+                    Object dependency = getInstance(dependencyType);
+                    field.setAccessible(true);
+                    field.set(instance, dependency);
+                }
+
+            }
+            return instance;
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to instantiate "
+                    + interfaceClazz.getName(), e);
+        }
     }
 }
